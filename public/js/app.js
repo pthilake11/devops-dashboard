73708554f7254e7cf9bb6ef0c465420417774b79
Praveen Thilakendranath <praveen_t@infosys.com>
2017-02-20 12:43:02 +0530

    commit more changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'LoginCtrl','DetailsCtrl','ApiService','nvd3']).run(function($rootScope){
 
     $rootScope.copyright = '© Copyright 2017. AutoBots Team';
+    $rootScope.failure = false;
     //$rootScope.authenticated = false;
     //$rootScope.applications = [];
 
